07a87bf1fa2e63856ec537709daa0669a3626e49
kaka <ntnghia.ngo@gmail.com>
2023-10-10 00:57:25 +0700

    ao

--- src/api/v1/routes/admin.route.js
+++ src/api/v1/routes/admin.route.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-
 const { createApiKeyHandler } = require("../controllers/admin.controller");
 const { _requireRole } = require("../helpers/role");
 const { checkApiKey } = require("../middleware/apikey.middleware");
@@ -7,12 +6,10 @@ const Auth = require("../middleware/Auth");
 
 const router = Router();
 
-router.use(
-  "/apikey/create",
-  checkApiKey,
-  _requireRole("ADMIN"),
-  Auth.checkRole,
-  createApiKeyHandler
-);
+router.use(checkApiKey);
+router.use(_requireRole("ADMIN"));
+router.use(Auth.checkRole);
+
+router.post("/apikey/create", createApiKeyHandler);
 
 module.exports = router;
